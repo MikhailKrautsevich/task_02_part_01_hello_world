805deb1547af157fc0f1b11698b4a7399ac9e928
Misha <mihail4egevara@gmail.com>
2022-04-04 16:14:18 +0300

    1.1 - Android Studio and Hello World (HomeWork)

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private val LOG_TAG : String = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(LOG_TAG, "Hello World (debug)")
+        Log.e(LOG_TAG, "Hello World (error)")
+//        Log.w(LOG_TAG, "Hello World (warn)")
+//        Log.i(LOG_TAG, "Hello World (info)")
     }
 }
